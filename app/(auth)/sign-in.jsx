3c48cc949904eb794e8d69e7c137ea7971511489
Karl George <karlgeorge87@gmail.com>
2024-04-25 11:14:31 +0200

    Style sign in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import { View, Text, ScrollView, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 const SignIn = () => {
   const [form, setForm] = useState({ email: '', password: '' });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = (second) => {
+    third;
+  };
 
   return (
     <SafeAreaView className='h-full bg-primary'>
@@ -32,6 +39,25 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles='mt-7'
           />
+
+          <CustomButton
+            title='Sign In'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex-row justify-center gap-2 pt-5'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Don't have an account?
+            </Text>
+            <Link
+              href='/sign-up'
+              className='text-lg font-psemibold text-secondary'
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
