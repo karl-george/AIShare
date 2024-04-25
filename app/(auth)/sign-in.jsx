3e0ab7a6ff7afdd4c21c0326c46061ea3ac4952f
Karl George <karlgeorge87@gmail.com>
2024-04-25 10:54:09 +0200

    Add styling and form

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { useState } from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
 
 const SignIn = () => {
+  const [form, setForm] = useState({ email: '', password: '' });
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='h-full bg-primary'>
+      <ScrollView>
+        <View className='justify-center w-full h-full px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[115px] h-[35px]'
+          />
+          <Text className='mt-10 text-2xl text-white font-psemibold'>
+            Log in to Aora
+          </Text>
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
