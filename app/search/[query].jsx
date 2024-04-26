0d961cd7f32f8d92c2929982a1c1acab882cfa4d
Karl George <karlgeorge87@gmail.com>
2024-04-26 15:32:23 +0200

    Add search screen styling

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,14 +1,46 @@
-import { View, Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-// Dynamic route
+import EmptyState from '../../components/EmptyState';
+import SearchInput from '../../components/SearchInput';
+import VideoCard from '../../components/VideoCard';
+import { searchPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
 
 const Search = () => {
+  const [refreshing, setRefreshing] = useState(false);
   const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
     <SafeAreaView className='bg-primary h-full'>
-      <Text className='text-3xl text-white'>{query}</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className='my-6 px-4'>
+            <Text className='font-pmedium text-sm text-gray-100'>
+              Search Results
+            </Text>
+            <Text className='text-2xl font-psemibold text-white'>{query}</Text>
+            <View className='mt-6 mb-8'>
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title='No videos found'
+            subtitle='No videos found for this search query'
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
