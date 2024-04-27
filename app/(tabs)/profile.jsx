186eeb80529c6de985833a2f82da53e85682fcc3
Karl George <karlgeorge87@gmail.com>
2024-04-27 11:14:45 +0200

    Add profile screen styling

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import EmptyState from '../../components/EmptyState';
+import InfoBox from '../../components/InfoBox';
+import VideoCard from '../../components/VideoCard';
+import { icons } from '../../constants';
+import { useGlobalContext } from '../../context/GlobalProvider';
+import { getUserPosts, signOut } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import { router } from 'expo-router';
 
 const Profile = () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(() => getUserPosts(user.$id));
+
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+    router.replace('/sign-in');
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className='h-full bg-primary'>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className='items-center justify-center w-full px-4 mt-6 mb-12'>
+            <TouchableOpacity
+              className='items-end w-full mb-10'
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode='contain'
+                className='w-6 h-6'
+              />
+            </TouchableOpacity>
+            <View className='items-center justify-center w-16 h-16 border rounded-lg border-secondary'>
+              <Image
+                source={{ uri: user?.avatar }}
+                className='w-[90%] h-[90%] rounded-lg'
+                resizeMode='cover'
+              />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles='mt-5'
+              titleStyles='text-lg'
+            />
+            <View className='flex-row mt-5'>
+              <InfoBox
+                title={posts.length || 0}
+                subtitle='Posts'
+                containerStyles='mr-10'
+                titleStyles='text-xl'
+              />
+              <InfoBox
+                title='12.5k'
+                subtitle='Followers'
+                titleStyles='text-xl'
+              />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title='No videos found'
+            subtitle='No videos found for this search query'
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
 export default Profile;
-
-const styles = StyleSheet.create({});
